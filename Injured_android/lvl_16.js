71ce7fdac654a11dff400359b1ee1351e8fcce0a
Joy2225 <archishman9892@gmail.com>
2025-03-17 23:44:00 +0530

    Level 16 solved, although unintended way.

--- Injured_android/lvl_16.js
+++ Injured_android/lvl_16.js
@@ -1,29 +1,9 @@
 Java.perform(function() {
-
-    let CSPBypassActivity = Java.use("b3nac.injuredandroid.CSPBypassActivity");
-    CSPBypassActivity["$init"].implementation = function () {
-        console.log(`CSPBypassActivity.$init is called`);
-        this["$init"]();
+    let g = Java.use("d.s.d.g");
+    g["a"].implementation = function (obj, obj2) {
+        console.log(`g.a is called: obj=${obj}, obj2=${obj2}`);
+        let result = this["a"](obj, obj2);
+        console.log(`g.a result=${result}`);
+        return result;
     };
-
-    let m = Java.use("b3nac.injuredandroid.CSPBypassActivity");
-m["M"].implementation = function () {
-    console.log(`CSPBypassActivity.M is called`);
-    this["M"]();
-};
-
-let l = Java.use("b3nac.injuredandroid.CSPBypassActivity");
-l["L"].implementation = function () {
-    console.log(`CSPBypassActivity.L is called`);
-    this["L"]();
-};
-
-let create = Java.use("b3nac.injuredandroid.CSPBypassActivity");
-create["onCreate"].implementation = function (bundle)  {
-    console.log(`CSPBypassActivity.onCreate is called`);
-    this["onCreate"](bundle);
-};
-
-}
-
-);
+});
